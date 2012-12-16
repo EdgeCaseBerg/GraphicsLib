647a9a51a31c71a85ec956d0e3fa816a5b0975b9
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2012-12-16 01:57:03 -0800

    Began reading about camera transforms and other things, going to try to make a simple 3d object show up. black triangle as it were

---
+++ DrawingPane3D.java
@@ -0,0 +1,23 @@
+package GraphicsLib;
+
+
+import javax.swing.SwingUtilities;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.BorderFactory;
+
+import java.awt.Color;
+import java.awt.Dimension;
+
+/**
+*@author Ethan Eldridge <ejayeldridge @ gmail.com>
+*@version 0.0
+*@since 2012-12-16
+*
+* Defines a Cartesion 3D Space to draw on
+* Theme music for this project: http://endlessvideo.com/watch?v=t2ZRy71vivk (Listened on first night for 5+ hours)
+*/
+public class DrawingPane3D extends JFrame{
+	
+
+}
